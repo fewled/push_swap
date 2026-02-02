f39e12f5e1e33be04b59de06c6dc78c9f3e3daf5
fewled <fewled@proton.me>
2026-02-02 14:37:15 +0100

    [>] incomplete constructor

--- tools/constructors.c
+++ tools/constructors.c
@@ -6,18 +6,28 @@
 /*   By: vpolard <vpolard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 18:02:31 by vpolard           #+#    #+#             */
-/*   Updated: 2026/01/29 17:32:24 by vpolard          ###   ########.fr       */
+/*   Updated: 2026/02/02 14:35:56 by vpolard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tools.h"
 
-static int	parse_arguments(t_package *package)
+static int	parse_string(t_package *package)
 {
-	// 1. Handle the use of the ARG variable
-	// 2. Handle the use of multiple numbers as parameter
-	(void)package;
-	return (0);
+}
+
+static int	parse_list(t_package *package)
+{
+}
+
+static int	setup_stacks(t_package *package)
+{
+	package->a = (t_stack *)0;
+	package->b = (t_stack *)0;
+	if (!(package->a = malloc(sizeof(t_stack)))
+		|| !(package->b = malloc(sizeof(t_stack))))
+		return (0);
+	// To complete
 }
 
 t_package	*new_package(int arg_count, char **arg_list)
@@ -27,12 +37,10 @@ t_package	*new_package(int arg_count, char **arg_list)
 	if (!(package = malloc(sizeof(t_package))))
 		return ((t_package *)0);
 	package->size = arg_count - 1;
-	package->data = (arg_list + 1);
-	package->a = (t_stack *)0;
-	package->b = (t_stack *)0;
-	if (!(package->a = malloc(sizeof(t_stack))) 
-			|| !(package->b = malloc(sizeof(t_stack)))
-			|| !parse_arguments(package))
-		return (clean(package), (t_package *)0);
+	package->data = &(arg_list + 1);
+
+	// At this point check for argument length and format
+	// Allocate stacks accordingly
+	
 	return (package);
 }
