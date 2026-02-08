476c4e9684f0f73b0aef866e8c8c988787765b90
fewled <fewled@proton.me>
2026-02-08 18:54:39 +0100

    [+] sorting steps

--- algo/sort.c
+++ algo/sort.c
@@ -6,7 +6,7 @@
 /*   By: vpolard <vpolard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 17:52:48 by vpolard           #+#    #+#             */
-/*   Updated: 2026/02/08 17:54:12 by vpolard          ###   ########.fr       */
+/*   Updated: 2026/02/08 18:53:30 by vpolard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	sort(t_package *package)
 {
-	(void)package;
-	ft_putstr("[i] Calling sort !\n");
+	while (package->a->top > 3)
+	{
+		// Transfer wisely from a to b
+	}
+	// Sort the three remaining numbers inside of a
+	while (package->b->top > 0)
+	{
+		// Transfer back from b to a
+	}
+	// Bring the smallest number at the top of the stack
 }
