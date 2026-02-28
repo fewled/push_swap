aa7329b14a8bfa51aa06ce981c59195aff7e7bc8
fewled <fewled@proton.me>
2026-02-28 16:28:08 +0100

    [+] get_closest

--- src/algo/transfer.c
+++ src/algo/transfer.c
@@ -12,21 +12,39 @@
 
 #include "algo.h"
 
-void    get_closest(t_package *package);
-/*
+void    get_closest(t_package *package)
+{
+	int	closest;
+	int	min_diff;
+	int	current_diff;
+	int	index;
+
+	closest = package->b->top;
+	min_diff = ft_abs(package->b->content[closest]
+		- package->a->content[package->a->pin]);
+	index = package->b->top;
+	while (index >= 0)
+	{
+		current_diff = ft_abs(package->b->content[index]
+			- package->a->content[package->a->pin]);
+		if (current_diff < min_diff)
+		{
+			min_diff = current_diff;
+			closest = index;
+		}
+		index--;
+	}
+	package->b->pin = closest;
+}
+
 void    get_directions(t_package *package);
 void	get_individual_cost(t_package *package);
 void	get_compared_cost(t_package *package);
 void	is_current_best(t_package *package);
 void	apply_move(t_package *package);
-*/
 
 void    transfer(t_package *package)
 {
-    package->a->pin = package->a->top;
-    get_closest(package);
-    printf("[i] Closest : %d\n", package->current->bval);
-    /*
     while (package->a->top > 2)
     {
         package->a->pin = package->a->top;
@@ -41,5 +59,4 @@ void    transfer(t_package *package)
         }
         apply_move(package);
     }
-    */
 }
