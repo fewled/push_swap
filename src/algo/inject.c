10450b592d8974e9d6e43aca3274cc9b9f425578
fewled <fewled@proton.me>
2026-03-08 17:42:27 +0100

    [+] reversed injection

--- src/algo/inject.c
+++ src/algo/inject.c
@@ -6,16 +6,16 @@
 /*   By: vpolard <vpolard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 09:27:18 by vpolard           #+#    #+#             */
-/*   Updated: 2026/02/28 09:27:19 by vpolard          ###   ########.fr       */
+/*   Updated: 2026/03/08 17:41:44 by vpolard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algo.h"
 
-// void    inject(t_package *package)
-// {
-//     if (package->a->top >= 3)
-//         pb(package);
-//     if (package->a->top >= 4)
-//         pb(package);
-// }
+void    inject(t_package *package)
+{
+    if (package->a->btm >= 4)
+        pb(package);
+    if (package->a->btm >= 3)
+        pb(package);
+}
--- src/algo/sort.c
+++ src/algo/sort.c
@@ -6,7 +6,7 @@
 /*   By: vpolard <vpolard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 17:52:48 by vpolard           #+#    #+#             */
-/*   Updated: 2026/03/08 17:33:50 by vpolard          ###   ########.fr       */
+/*   Updated: 2026/03/08 17:37:17 by vpolard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	sort(t_package *package)
 {
-	(void)package;
-    // inject(package);
+    inject(package);
+	status(package);
 	// transfer(package);
 	// arrange(package);
 	// refill(package);
