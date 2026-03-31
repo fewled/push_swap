6b4b7121a95ccccd4a3e3a063647da1d5d864b47
fewled <fewled@proton.me>
2026-03-31 15:41:04 +0200

    [~] before parsing

--- rust/src/package/new.rs
+++ rust/src/package/new.rs
@@ -5,13 +5,22 @@ pub struct Package {
 }
 
 impl Package {
-    pub fn new() -> Self {
+    pub fn new() -> Result<Self, String> {
         let raw_args: Vec<String> = args().skip(1).collect();
-        if raw_args.len() < 1 {
-            return Self { args: None };
+        let args: Result<Vec<String>, String>;
+        if raw_args.len() == 1 {
+            args = stacked_parse(raw_args[0]);
+        } else if raw_args.len() > 1 {
+            args = splitted_parse(raw_args);
         }
-        Self {
-            args: Some(raw_args),
+        match args {
+            Ok(content) => Ok(Self {
+                args: Some(content),
+            }),
+            Err(msg) => Err(msg),
         }
     }
 }
+
+fn stacked_parse(argument: String) -> Result<Vec<String>, String> {}
+fn splitted_parse(arguments: Vec<String>) -> Result<Vec<String>> {}
