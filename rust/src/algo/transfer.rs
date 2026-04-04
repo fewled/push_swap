7b6130c215ac36ddfa8909b534ed343070fb819a
fewled <fewled@proton.me>
2026-04-04 16:04:01 +0200

    [+] compare_moves

--- rust/src/algo/transfer.rs
+++ rust/src/algo/transfer.rs
@@ -10,14 +10,13 @@ impl Package {
                     .get_directions()
                     .get_individual_cost()
                     .get_compared_cost()
-                    .compare_moves()
-                    .apply_move();
+                    .compare_moves();
                 index += 1;
             }
+            self.apply_move();
         }
         self
     }
-
     fn get_closest(&mut self, index: usize) -> &mut Self {
         let mut min_diff: i32 = self.a.content[index] - self.b.content[index];
         self.current.apin = index;
@@ -47,11 +46,14 @@ impl Package {
     fn get_individual_cost(&mut self) -> &mut Self {
         match self.current.adir {
             true => self.current.acost = self.current.apin,
-            false => self.current.acost = self.a.content.len() - self.current.apin + 1,
+            false => self.current.acost = (self.a.content.len() - 1) - self.current.apin + 1,
         }
         match self.current.bdir {
             true => self.current.bcost = self.current.bpin,
-            false => self.current.bcost = self.b.content.len() - self.current.bpin + 1,
+            false => self.current.bcost = (self.b.content.len() - 1) - self.current.bpin + 1,
+        }
+        if self.current.aval < self.current.bval {
+            self.current.bcost += 1;
         }
         self
     }
@@ -59,6 +61,17 @@ impl Package {
         self
     }
     fn compare_moves(&mut self) -> &mut Self {
+        if self.current.cost < self.best.cost || self.best.cost == 0 {
+            self.best.cost = self.current.cost;
+            self.best.acost = self.current.acost;
+            self.best.bcost = self.current.bcost;
+            self.best.aval = self.current.aval;
+            self.best.bval = self.current.bval;
+            self.best.apin = self.current.apin;
+            self.best.bpin = self.current.bpin;
+            self.best.adir = self.current.adir;
+            self.best.bdir = self.current.bdir;
+        }
         self
     }
     fn apply_move(&mut self) {}
